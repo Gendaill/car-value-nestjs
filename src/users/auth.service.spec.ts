dbe36883e1b663f9e9af6e61cc48ccc8a76f27cf
James Diaz <James7501@hotmail.com>
2023-08-27 15:14:37 -0500

    Add e2e test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -9,10 +9,21 @@ describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
     // create a facke copy of the user service
+    const users: User[] = [];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteruser = users.filter((user) => user.email === email);
+        return Promise.resolve(filteruser);
+      },
+      create: (email: string, password: string) => {
+        const newuser = {
+          id: Math.floor(Math.random() * 99999),
+          email,
+          password,
+        } as User;
+        users.push(newuser);
+        return Promise.resolve(newuser);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -41,26 +52,28 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a@a.com', password: '1234' } as User]);
-    await expect(service.signup('a@a.com', '1234')).rejects.toBeInstanceOf(
+    await service.signup('asdf@asdf.com', 'asdf');
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
   });
 
   it('throws if signin is called with an unused email', async () => {
-    await expect(service.signin('a@a.com', '1234')).rejects.toThrow(
-      NotFoundException,
-    );
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
   });
 
   it('throws if an invalid password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
-      ]);
+    await service.signup('laskdjf@alskdfj.com', 'password');
     await expect(
-      service.signin('laskdjf@alskdfj.com', 'passowrd'),
+      service.signin('laskdjf@alskdfj.com', 'laksdlfkj'),
     ).rejects.toThrow(BadRequestException);
+  });
+
+  it('returns a user if the correct email & password is provided', async () => {
+    await service.signup('test@test.com', 'password123');
+    const user = await service.signin('test@test.com', 'password123');
+    expect(user).toBeDefined();
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   Session,
   UseGuards,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -54,8 +55,10 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   @Get()
